41285245dd22e00d3f3f655f87581cb09404a1b0
leflores <pflores.fisi22@gmail.com>
2023-01-20 04:39:10 -0500

    add .scripts dir

---
+++ .scripts/setup.sh
@@ -0,0 +1,22 @@
+git clone --bare https://github.com/leflores-fisi/dotfiles $HOME/.dotfiles
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+
+# Try to checkout (and resolve conflicts)
+dotfiles checkout
+if [ $? -ne 0 ]; then
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "^\s" | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    echo "Backup done. See .dotfiles.backup"
+fi;
+
+echo "Checked out config. Enjoy! (after fixing it)"
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
+
+# Modified from https://bitbucket.org/durdn/cfg/raw/master/.bin/install.sh
