ea4600c969b3dd03a1903a59c84f067adc1eef30
leflores <pflores.fisi22@gmail.com>
2023-03-03 20:09:22 -0500

    setup: fix typo

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -8,15 +8,17 @@ mkdir -p .dotfiles-backup
 
 # Try to checkout (and resolve conflicts)
 dotfiles checkout
+
 if [ $? -ne 0 ]; then
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "^\s" | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "^\s" | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
     echo "Backup done. See .dotfiles.backup"
-fi;
+fi
 
 echo "Checked out config. Enjoy! (after fixing it)"
 
 dotfiles checkout
+
 dotfiles config status.showUntrackedFiles no
 
 # Modified from https://bitbucket.org/durdn/cfg/raw/master/.bin/install.sh
