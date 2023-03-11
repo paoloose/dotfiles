f2636ad13725b7e5dfaf4858acab6a3e4e7ab57d
Paolo Flores <pflores.fisi22@gmail.com>
2023-03-11 00:11:12 -0500

    rofi: add rofi configuration

---
+++ .config/rofi/launcher.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
+#
+
+## Run
+rofi \
+    -modi "combi" \
+    -combi-modi "drun,window" \
+    -show combi \
+    -theme launcher.theme.rasi \
+    -sort
