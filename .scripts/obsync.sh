f7690df6c74d2a34c9c2c6eab90eb8a3007aa5f8
Paolo Flores <pflores.fisi22@gmail.com>
2025-03-30 23:40:58 -0500

    improve error handling for obsync.sh

--- .scripts/obsync.sh
+++ .scripts/obsync.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+: """
+Obsidian wrapper script that
+- Stash local changes
+- Pull changes from upstream
+- Run obsidian
+- Commit and push new changes
+
+Replace your Obsidian Exec line manually in your obsidian.desktop
+
+    seticon.sh list | grep -i obsidian
+    micro /usr/share/applications/obsidian.desktop
+
+    Exec=/path/to/obsync.sh [device_name] %U
+"""
+
 set -eE
 
 OBSIDIAN_DIR="/home/paolo/Documents/ObsidianVault"
@@ -35,10 +50,10 @@ error_exit() {
     exit 1
 }
 
-shift 1
+shift 1 || error_exit "No environment name provided. Run with obsync.sh [environment name]"
 
 function cleanup {
-    $alert_cmd "It is no clear what happened. But I cannot say it was successful. Pretty much an error yeah. Script was aborted."
+    error_exit "It is no clear what happened. But I cannot say it was successful. Pretty much an error yeah. Script was aborted."
 }
 
 trap 'cleanup' ERR
@@ -46,9 +61,9 @@ trap 'cleanup' ERR
 cd $OBSIDIAN_DIR
 git stash || error_exit "Failed to stash changes"
 git pull origin main || error_exit "Failed to pull changes from upstream"
-git stash pop || error_exit "Failed to pop stash"
-$OBSIDIAN_BIN $@ || error_exit "Failed to start obsidian"
+git stash pop || :
+$OBSIDIAN_BIN $@ || error_exit "Obsidian just closed unexpectedly. Changes will be saved anyway" || :
 
-git add .
-git commit -m "latest notes for $environment"
+git add . || error_exit "git add command failed"
+git commit -m "latest notes for $environment" || error_exit "Nothing to commit"
 git push origin main || error_exit "Failed to push changes"
